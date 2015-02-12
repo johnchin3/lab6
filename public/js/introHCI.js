2979cd6d522b61ac11dda2c4de33194aefe87510
vagrant <vagrant@precise32.(none)>
2015-02-12 22:37:29 +0000

    lab6 done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,8 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, addProject);
 }
 
 /*
@@ -35,4 +37,30 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", randomColor);
 }
+
+function randomColor(result){
+	console.log(result);
+
+	var colors = result["colors"]['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
+}
+
+function addProject(result){
+	console.log(result);
+
+	var projectHTML = '<a href="#" class="thumbnail">' +
+		'<img src="' + result['image'] + '" class="detailsImage">' +
+		'<p>' + result['title'] + '</p>' + '<p><small>'
+		+ result['date'] + '</small></p></a>';
+
+	$('#project'+result['id']+' .details').html(projectHTML);
+}
+
